66ec369bc66af4f3a75fea823a956b3b07d770d4
fspiritosi <fspiritosi@gmail.com>
2023-02-25 14:31:28 -0300

    para presentacion

---
+++ back/src/app.js
@@ -0,0 +1,51 @@
+const { default: axios } = require('axios');
+const { json } = require('body-parser');
+const express = require('express');
+const { getCharById } = require('./controllers/getCharById');
+const app = express();
+
+const PORT = 3001;
+const fav = [];
+
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+
+
+
+
+//routes
+
+app.get("//rickandmorty/character/:{id}", async (req, res)=> {
+     
+    const id = req.params
+
+    getCharById(res, id)
+   
+});
+
+app.get("/rickandmorty/detail/:{detailId}", async (req, res) => {
+    const detailCharacter = await axios.get(`https://rickandmorty/detail/${detailId}`);
+
+    let charDetail = {
+        name: detailCharacter.name,
+        status: detailCharacter.status,
+        species: detailCharacter.species,
+        gender: detailCharacter.gender,
+        origin: detailCharacter.origin,
+        image: detailCharacter.image
+    }
+
+    res.json(detailCharacter)
+})
+
+app.get("/rickandmorty/fav", (req, res) => {});
+
+app.post("/rickandmorty/fav", (req, res) => {});
+
+app.delete("/rickandmorty/fav/${id}", (req, res) => {});
+
+app.listen(PORT, () => {
+    console.log(`Server run on port ${PORT}`);
+})
--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -1,23 +1,46 @@
 const axios = require('axios')
 
-const getCharById = (res, id) => {
-    axios(`https://rickandmortyapi.com/api/character/${id}`)
-        .then(result => result.data)
-        .then(data => {
-           let character = {
-                id: data.id,
-                image: data.image,
-                name: data.name,
-                gender: data.gender,
-                species: data.species
-           }
-           res.writeHead(200,  {'Content-type' : 'application/json' })
-           res.end(JSON.stringify(character))
-        })
-        .catch(err => 
-            res.writeHead(500, {'Content-type' : 'text/plain'})
-            .end(`Character with ${id} ID not found`)
-            )
-};
+//Promise method
+
+// const getCharById = (res, id) => {
+//     axios(`https://rickandmortyapi.com/api/character/${id}`)
+//         .then(result => result.data)
+//         .then(data => {
+//            let character = {
+//                 id: data.id,
+//                 image: data.image,
+//                 name: data.name,
+//                 gender: data.gender,
+//                 species: data.species
+//            }
+//            res.writeHead(200,  {'Content-type' : 'application/json' })
+//            res.end(JSON.stringify(character))
+//         })
+//         .catch(err => 
+//             res.writeHead(500, {'Content-type' : 'text/plain'})
+//             .end(`Character with ${id} ID not found`)
+//             )
+// };
+
+//async method
+
+const getCharById = async (req, res) => {
+    try{
+        const char = await axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+
+    let character = {
+        id: char.data.id,
+        name: char.data.name,
+        species: char.data.species,
+        gender: char.data.gender,
+        image: char.data.image
+    }
+    res.status(200).send(character)
+
+    }catch(err){
+        res.status(400).send(err)
+    }
+
+}
 
 module.exports = { getCharById };
